e1d26527074695016b1518c09f1f4f8bb753f706
Wesley <31936237+wesleychildress@users.noreply.github.com>
2020-02-12 10:27:11 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <$php
    define('DB_SERVER', 'localhost');
    define('DB_USERNAME', 'administrator');
-   define('DB_PASSWORD', 'H@L01halo');
+   define('DB_PASSWORD', '****');
    define('DB_DATABASE', 'haloGuests');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
